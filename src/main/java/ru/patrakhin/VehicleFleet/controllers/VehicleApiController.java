218d5f9017e7049c8b2a115977e83cc09f16247a
patrakhin <patrakhin@mail.ru>
2023-08-06 20:08:55 +0300

    added RestController 200806082023

---
+++ src/main/java/ru/patrakhin/VehicleFleet/controllers/VehicleApiController.java
@@ -0,0 +1,25 @@
+package ru.patrakhin.VehicleFleet.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.patrakhin.VehicleFleet.dto.VehiclesDTO;
+import ru.patrakhin.VehicleFleet.services.VehicleService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/vehicles")
+public class VehicleApiController {
+
+    private final VehicleService vehicleService;
+
+    public VehicleApiController(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
+    }
+
+    @GetMapping()
+    public List<VehiclesDTO> getAllVehicles() {
+        return vehicleService.getAllVehicles();
+    }
+}
