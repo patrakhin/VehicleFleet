92053f36b89841b7c693a0c0c1161b007fec69b9
patrakhin <patrakhin@mail.ru>
2023-08-27 17:10:03 +0300

    fixed SecurityConfig 170927082023

--- src/main/java/ru/patrakhin/VehicleFleet/config/SecurityConfig.java
+++ src/main/java/ru/patrakhin/VehicleFleet/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import ru.patrakhin.VehicleFleet.services.PersonDetailService;
 
 
@@ -30,7 +31,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http)throws Exception{
         //конфигурир сам спринг секьюрити и авторизацию
         http
-                .csrf().disable() //откл защ от межсайт поддел запросов
+                //.csrf().disable()//откл защ от межсайт поддел запросов .csrf().disable()
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()) // включаем CSRF защиту
+                .and()// союз и для сцепки настроек (создания цепочки)
                 .authorizeRequests()                                         //настр
                 .antMatchers("/manager1/**").hasRole("MANAGER1")
                 .antMatchers("/manager2/**").hasRole("MANAGER2")
