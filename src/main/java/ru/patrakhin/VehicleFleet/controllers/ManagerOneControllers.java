7c6093e23e1eb3ecaecbe0ef3965058adf28df2a
patrakhin <patrakhin@mail.ru>
2023-09-02 16:19:58 +0300

    added CRUD from enterprise 161902092023

--- src/main/java/ru/patrakhin/VehicleFleet/controllers/ManagerOneControllers.java
+++ src/main/java/ru/patrakhin/VehicleFleet/controllers/ManagerOneControllers.java
@@ -22,8 +22,6 @@ import ru.patrakhin.VehicleFleet.services.ManagersService;
 import ru.patrakhin.VehicleFleet.services.VehicleService;
 import ru.patrakhin.VehicleFleet.util.EnterpriseErrorResponse;
 import ru.patrakhin.VehicleFleet.util.EnterpriseNotCreatedException;
-import ru.patrakhin.VehicleFleet.util.EnterpriseNotFoundException;
-import ru.patrakhin.VehicleFleet.util.VehicleNotCreatedException;
 
 import java.util.List;
 
@@ -59,25 +57,6 @@ public class ManagerOneControllers {
         return managersService.getVehiclesIdsByEnterprisesId(managerId);
     }
 
-    @PostMapping("/vehicles/new")
-    public List <VehiclesDTO> createVehicle(@RequestBody VehiclesDTO vehiclesDTO, BindingResult bindingResult) {
-        // Создание новой машинки
-        if (bindingResult.hasErrors()) {
-            StringBuilder errorMsg = new StringBuilder();
-            List<FieldError> errors = bindingResult.getFieldErrors();
-            for (FieldError error : errors) {
-                errorMsg.append(error.getField())
-                        .append(" - ")
-                        .append(error.getDefaultMessage())
-                        .append(";");
-            }
-            throw new VehicleNotCreatedException(errorMsg.toString());
-        }
-        System.out.println(vehiclesDTO);
-        vehicleService.addVehicle(vehiclesDTO); //сохранили новую машинку
-        return getAllVehicles(); //вывели весь список машинок
-    }
-
     @GetMapping("/drivers")
     @ResponseBody
     public List<DriversDTO> getAllDrivers(Authentication authentication){
@@ -95,8 +74,8 @@ public class ManagerOneControllers {
     }
 
     @PostMapping("/enterprises/new")
-    public List <EnterprisesDTO> createEnterprise(@RequestBody EnterprisesDTO enterprisesDTO, BindingResult bindingResult) {
-        // Создание нового предприятия
+    public List <EnterprisesDTO> create(@RequestBody EnterprisesDTO enterprisesDTO, BindingResult bindingResult) {
+        // Создание нового предприятия //вернулся
         if (bindingResult.hasErrors()) {
             StringBuilder errorMsg = new StringBuilder();
             List<FieldError> errors = bindingResult.getFieldErrors();
@@ -114,45 +93,9 @@ public class ManagerOneControllers {
         Managers managers = new Managers();
         managers.setPerson(userDetails.getPerson());
         managers.setEnterprises(enterprisesService.getEnterpriseByName(enterprisesDTO));
-        managerRepository.save(managers); //сохранили связь менедж и предприятия
+        managerRepository.save(managers);
         Integer managerId = userDetails.getId();
         return managersService.getEnterpriseIdsByPersonId(managerId);
-    }
-
-    @PutMapping("/enterprises/{id}")
-    public EnterprisesDTO updateEnterprise(@PathVariable("id") int id, @RequestBody EnterprisesDTO enterprisesDTO,
-                                           BindingResult bindingResult) {
-        // Обновление данных предприятия по id
-        if (bindingResult.hasErrors()) {
-            StringBuilder errorMsg = new StringBuilder();
-            List<FieldError> errors = bindingResult.getFieldErrors();
-            for (FieldError error : errors) {
-                errorMsg.append(error.getField())
-                        .append(" - ")
-                        .append(error.getDefaultMessage())
-                        .append(";");
-            }
-            throw new EnterpriseNotCreatedException(errorMsg.toString());
-        }
-        Enterprises existingEnterprises = enterprisesService.getEnterprisesById(id);
-        if (existingEnterprises == null) {
-            throw new EnterpriseNotFoundException("Enterprises with this id wasn't found!");
-        }
-        Enterprises updatedEnterprises = convertToEnterprises(enterprisesDTO);
-        updatedEnterprises.setId(id);
-        enterprisesService.updateEnterprise(id, enterprisesDTO); //уже сделанная функция обновления в сервисе
-        return convertToEnterprisesDTO(enterprisesService.getEnterprisesById(id));
-    }
-
-    @DeleteMapping("enterprises/delete/{id}")
-    public EnterprisesDTO deleteEnterprise(@PathVariable("id") int id) {
-        // Удаление предприятия по id
-        Enterprises existingEnterprise = enterprisesService.getEnterprisesById(id);
-        if (existingEnterprise == null) {
-            throw new EnterpriseNotFoundException("Enterprises with this id wasn't found!");
-        }
-        enterprisesService.deleteEnterprise(id);
-        return convertToEnterprisesDTO(existingEnterprise);
     }
 
     @ExceptionHandler
